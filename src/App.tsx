8a83d30d05d8d244fe6e84fc2a19a246a97ff2a4
Sai priya Muthamsetty <86355166+saipriyamuthamsetty-zessta@users.noreply.github.com>
2022-02-22 19:13:44 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Demo React App.
+          Demo React App. 
+          Testing
         </p>
         <a
           className="App-link"
