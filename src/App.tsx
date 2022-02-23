db5b4540ab2bccff994b42705856373df5103961
Sai priya Muthamsetty <86355166+saipriyamuthamsetty-zessta@users.noreply.github.com>
2022-02-23 18:41:56 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Deploy code
+          Deployment testing
         </a>
       </header>
     </div>
